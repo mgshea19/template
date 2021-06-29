312d89e8678af99df2087ecd17fbc6b8030f4391
mgshea19 <mgshea@bu.edu;git config --global --listgit clone https://github.com/mgshea19/templategit config --global user.email mgshea@bu.edu>
2021-06-29 16:32:09 -0400

    Create MS Template

---
+++ RunScript.R
@@ -0,0 +1,8 @@
+#Install/load packages
+if (!require("rmarkdown")) {install.packages("rmarkdown")}
+if (!require("pacman")) {install.packages("pacman")}#install relevant packages
+pacman::p_load(rmarkdown,readxl,haven,Hmisc,foreign,lattice,janitor,dplyr,rstudioapi,stringr)
+project<-substr(rstudioapi::getActiveProject(),max(as.data.frame(stringr::str_locate_all(rstudioapi::getActiveProject(),"/"))$start)+1,nchar(rstudioapi::getActiveProject()))
+program_name<-substr(rstudioapi::getSourceEditorContext()$path,max(as.data.frame(stringr::str_locate_all(rstudioapi::getSourceEditorContext()$path,"/"))$start)+1,max(as.data.frame(stringr::str_locate_all(rstudioapi::getSourceEditorContext()$path,".Rmd"))$start)-1)
+location<-dirname(rstudioapi::getSourceEditorContext()$path)
+save(project, program_name, location, file = "meta.RData")
